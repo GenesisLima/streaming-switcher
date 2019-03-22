16f0e8c750bd2536f570a41f5f966fab6f43e396
Prof. Genesis Lima <profgenesislima@gmail.com>
2019-03-22 17:45:22 -0300

    debugging stop service methods.

--- src/main/java/org/ntvru/streamingswitcher/service/SwitcherService.java
+++ src/main/java/org/ntvru/streamingswitcher/service/SwitcherService.java
@@ -37,8 +37,9 @@ public class SwitcherService {
 	}
 
 	public boolean stopService() {
+		 System.out.println("stop call on "+this.getClass().getName());
 		StopServiceTask task = new StopServiceTask(taskService);
-		if(!isServiceRunning()) {
+		if(isServiceRunning()) {
 			synchronized(this) {
 				Thread thread = new Thread(task,"startService");
 				thread.stop();
